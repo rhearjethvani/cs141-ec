5031d4c4d927a487767cf15f23e7d06c4e346f70
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 10:28:37 -0700

    add DiskManager struct with constructor

--- disk_manager.go
+++ disk_manager.go
@@ -1,3 +1,15 @@
 // maps to java disk manager
 
 package main
+
+type DiskManager struct {
+	resourceManager *ResourceManager
+	nextFreeSector []int
+}
+
+func NewDiskManager(numDisks int) *DiskManager {
+	return &DiskManager {
+		resourceManager: NewResourceManager(numDisks),
+		nextFreeSector: make([]int, numDisks),
+	}
+}

fc52695daa4979d43407dc19c36017284912a9b7
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 09:42:24 -0700

    add Printer struct with constructor

--- printer.go
+++ printer.go
@@ -1,3 +1,15 @@
 // maps to java printer
 
 package main
+
+const PrintDelayMs = 2750
+
+type Printer struct {
+	ID int
+}
+
+func NewPrinter(id int) *Printer {
+	return &Printer {
+		ID: id,
+	}
+}

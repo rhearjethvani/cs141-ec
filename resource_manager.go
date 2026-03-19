ed7664b788a6520c4a7684e55d237e584c07f2d3
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 10:20:54 -0700

    add ResourceManager struct with constructor

--- resource_manager.go
+++ resource_manager.go
@@ -1,3 +1,18 @@
 // maps to java resource manager
 
 package main
+
+type ResourceManager struct {
+	isFree []bool
+}
+
+func NewResourceManager(count int) *ResourceManager {
+	isFree := make([]bool, count)
+	for i := range isFree {
+		isFree[i] = true
+	}
+
+	return &ResourceManager {
+		isFree: isFree,
+	}
+}

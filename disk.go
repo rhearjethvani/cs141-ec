188829bf3b05149aafc5e632429e6c4cc2f575f4
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 09:40:29 -0700

    add Disk struct with sector storage and constructor

--- disk.go
+++ disk.go
@@ -1,3 +1,18 @@
 // maps to java disk
 
 package main
+
+const NumSectors = 2048
+const DiskDelayMs = 800
+
+type Disk struct {
+	ID int
+	Sectors []string
+}
+
+func NewDisk(id int) *Disk {
+	return &Disk {
+		ID: id,
+		Sectors: make([]string, NumSectors),
+	}
+}

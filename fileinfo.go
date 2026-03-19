e2d7c1d29adb3a0c3e1f2f39a017368e63fb0b08
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 09:33:12 -0700

    add FileInfo struct with constructor

--- fileinfo.go
+++ fileinfo.go
@@ -1,3 +1,23 @@
 // maps to java fileinfo
 
 package main
+
+import "fmt"
+
+type FileInfo struct {
+	DiskNumber int
+	StartingSector int
+	FileLength int
+}
+
+func NewFileInfo(disk int, start int, length int) FileInfo {
+	return FileInfo {
+		DiskNumber: disk,
+		StartingSector: start,
+		FileLength: length
+	}
+}
+
+func (f FileInfo) String() string {
+	return fmt.Sprintf("[disk=%d start=%d len=%d]", f.DiskNumber, f.StartingSector, f.FileLength)
+}

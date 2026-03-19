bdd3761478f7e0b577ef45a80b95bc431bf1ae69
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 10:30:03 -0700

    add getNextFreeSector functionality to DiskManager

--- disk_manager.go
+++ disk_manager.go
@@ -13,3 +13,8 @@ func NewDiskManager(numDisks int) *DiskManager {
 		nextFreeSector: make([]int, numDisks),
 	}
 }
+
+// returns the next available sector index for a disk
+func (dm *DiskManager) GetNextFreeSector(disk int) int {
+	return dm.nextFreeSector[disk]
+}

81d4119a0565f9b390ac70e4292f6a8d7ba171e2
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 09:46:38 -0700

    add DirectoryManager struct with constructor

--- directory_manager.go
+++ directory_manager.go
@@ -1,3 +1,13 @@
 // maps to java directory manager
 
 package main
+
+type DirectoryManager struct {
+	files map[string]FileInfo
+}
+
+func NewDirectoryManager() *DirectoryManager {
+	return &DirectoryManager {
+		files: make(map[string]FileInfo),
+	}
+}

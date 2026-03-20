97b339d34decb0813b47f56d78221c2cc480caa8
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 19:23:38 -0700

    add PrintJob struct with constructor

--- print_job.go
+++ print_job.go
@@ -1,3 +1,13 @@
 // maps to java printjobthread
 
 package main
+
+type PrintJob struct {
+	FileName string
+}
+
+func NewPrintJob(fileName string) *PrintJob {
+	return &PrintJob {
+		FileName: fileName,
+	}
+}

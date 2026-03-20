5233d1c15fdeb500798a20671364955e6bee257b
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-20 10:00:37 -0700

    make DirectoryManager safe for concurrent access

--- directory_manager.go
+++ directory_manager.go
@@ -2,8 +2,11 @@
 
 package main
 
+import "sync"
+
 type DirectoryManager struct {
 	files map[string]FileInfo
+	mu sync.Mutex
 }
 
 func NewDirectoryManager() *DirectoryManager {
@@ -14,11 +17,17 @@ func NewDirectoryManager() *DirectoryManager {
 
 // enter inserts or updates file metadata
 func (dm *DirectoryManager) Enter(name string, info FileInfo) {
+	dm.mu.Lock()
+	defer dm.mu.Unlock()
+
 	dm.files[name] = info
 }
 
 // returns fileinfo and whether it exists
 func (dm *DirectoryManager) Lookup(name string) (FileInfo, bool) {
+	dm.mu.Lock()
+	defer dm.mu.Unlock()
+	
 	info, valid := dm.files[name]
 	return info, valid
 }

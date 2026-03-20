8c3266083140139ba8775371943e62a8c9763874
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-19 19:10:24 -0700

    add PrinterManager struct with constructor

--- printer_manager.go
+++ printer_manager.go
@@ -1,3 +1,13 @@
 // maps to java printer manager
 
 package main
+
+type PrinterManager struct {
+	resourceManager *ResourceManager
+}
+
+func NewPrinterManager(numPrinters int) *PrinterManager {
+	return &PrinterManager {
+		resourceManager: NewResourceManager(numPrinters),
+	}
+}

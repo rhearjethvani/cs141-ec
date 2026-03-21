51f36493e0ed96cc6e8a5a352ad931e100d0d591
Rhea Jethvani <rhearjethvani@gmail.com>
2026-03-20 17:09:08 -0700

    fixing startPrinterScheduler bug

--- printer_scheduler.go
+++ printer_scheduler.go
@@ -12,23 +12,25 @@ func StartPrinterScheduler(
 	printerManager *PrinterManager,
 	printWG *sync.WaitGroup,
 ) {
-	go func() {
-		for {
-			job := printQueue.Dequeue()
+	for range printers {
+		go func() {
+			for {
+				job := printQueue.Dequeue()
 
-			fmt.Println("Scheduling print job:", job.FileName, "priority:", job.Priority)
+				fmt.Println("Scheduling print job:", job.FileName, "priority:", job.Priority)
 
-			printerNum := printerManager.Request()
-			printer := printers[printerNum]
+				printerNum := printerManager.Request()
+				printer := printers[printerNum]
 
-			for i := 0; i < job.Info.FileLength; i++ {
-				sector := job.Info.StartingSector + i
-				data := disks[job.Info.DiskNumber].Read(sector)
-				printer.PrintLine(data)
+				for i := 0; i < job.Info.FileLength; i++ {
+					sector := job.Info.StartingSector + i
+					data := disks[job.Info.DiskNumber].Read(sector)
+					printer.PrintLine(data)
+				}
+
+				printerManager.Release(printerNum)
+				printWG.Done()
 			}
-
-			printerManager.Release(printerNum)
-			printWG.Done()
-		}
-	}()
+		}()
+	}
 }
